e0c32818ef5572a72f093be132eb67c0b9d0e89f
ttxiaotaoge <910893316@qq.com>
2026-02-09 12:00:47 +0800

    添加中文说明，修改飞书反馈方式

--- nanobot/channels/feishu.py
+++ nanobot/channels/feishu.py
@@ -276,7 +276,7 @@ class FeishuChannel(BaseChannel):
             msg_type = message.message_type
             
             # Add reaction to indicate "seen"
-            await self._add_reaction(message_id, "THUMBSUP")
+            await self._add_reaction(message_id, "OK")
             
             # Parse message content
             if msg_type == "text":
